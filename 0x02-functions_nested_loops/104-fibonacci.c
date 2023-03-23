1df8945d6d98354325cd24d81539bcecfd9c38d8
TebatsoGugu <tebatsostocks@gmail.com>
2023-03-23 07:45:55 -0700

    task fib98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - fibonacci <3
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
  *
- * Purpose - no hardcode
- *
- * Return:  (Success)
+ * Return: Always 0.
  */
-
 int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 98; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 97)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
